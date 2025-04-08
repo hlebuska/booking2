2443f11ef5e371e446abf12efb9c528e10d21012
Diyar Makhambetali <dmakhambetali0806@gmail.com>
2025-04-08 22:08:04 +0500

    authertification added: check redoc

--- backend/bs/serializers.py
+++ backend/bs/serializers.py
@@ -1,6 +1,8 @@
 from decimal import Decimal, InvalidOperation
 from rest_framework import serializers
 from .models import *
+from django.contrib.auth.models import User
+
 
 class BarberSerializer(serializers.ModelSerializer):
 
@@ -32,3 +34,18 @@ class BookingSerializer(serializers.ModelSerializer):
         if not value.isdigit() or len(value) < 11:
             raise serializers.ValidationError("Неверный номер телефона. Он должен содержать не менее 11 цифр.")
         return value
+    
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only = True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username = validated_data['username'],
+            password = validated_data['password']
+        )
+        return user
