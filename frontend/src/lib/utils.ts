56c070711f71efa679718904473d09634affe570
Васютинский Глеб~ <g.v.vass@gmail.com>
2025-04-14 13:02:39 +0500

    Dual slider for master schedule range

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -19,6 +19,16 @@ export const formatDuration = (duration: number): string => {
     return `${hours ? `${hours} ч.` : ''} ${minutes ? `${minutes} мин.` : ''}`.trim();
 };
 
+export const formatTime = (mins: number): string => {
+    const hours = Math.floor(mins / 60);
+    const minutes = mins % 60;
+
+    const paddedHours = String(hours).padStart(2, '0');
+    const paddedMinutes = String(minutes).padStart(2, '0');
+
+    return `${paddedHours}:${paddedMinutes}`;
+};
+
 export const breadcrumbNames: Record<string, string> = {
     '': 'Главная',
     branchSelect: 'Филиал',
@@ -103,11 +113,9 @@ axiosApiClient.interceptors.response.use(
     (response) => response,
     async (error) => {
         const originalRequest = error.config;
-        console.log(originalRequest);
 
         if (error.response?.status === 401 && !originalRequest._retry) {
             originalRequest._retry = true;
-            console.log('404 caught');
             try {
                 const refreshResponse = await refreshToken();
 
@@ -119,10 +127,8 @@ axiosApiClient.interceptors.response.use(
                 originalRequest.headers['Authorization'] = `Bearer ${newAccessToken}`;
                 return axiosApiClient(originalRequest); // retry request
             } catch (refreshError) {
-                console.error('Refresh failed, logging out user');
-                console.log(refreshError);
                 useStore.getState().setAccessToken(null); // or navigate to login page
-                // window.location.href = '/login';
+                window.location.href = '/login';
                 return Promise.reject(refreshError);
             }
         }
