ce60c74cfa0ff003bbbca31f4cf30626bb69e590
hlebuska <143942126+hlebuska@users.noreply.github.com>
2025-05-04 15:19:36 +0500

    Update settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'unsafe-default')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost").split(",")
+ALLOWED_HOSTS = ['*']
 
 CACHES = {
     'default': {
